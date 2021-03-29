53d838018fa71bff6b30b139c081e6e775531a2c
ArberGjonaj <agjonaj@ikubinfo.al>
2021-03-29 11:25:01 +0200

    Starting Implementation of the Burger Builder Container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 class App extends React.Component {
   render() {
     return <div className="App">
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder/>
       </Layout>
     </div>;
   }
