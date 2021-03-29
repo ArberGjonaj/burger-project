81ee0c5f780599ae32df4af432e7b352ae6f6cb3
ArberGjonaj <agjonaj@ikubinfo.al>
2021-03-29 11:06:21 +0200

    Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-
+import Layout from './components/Layout/Layout'
 class App extends React.Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return <div className="App">
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>;
   }
 }
 export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,9 @@
+import Aux from "../../hoc/Auxillary";
+const Layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>{props.children}</main>
+  </Aux>
+);
+
+export default Layout;
